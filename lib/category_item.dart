b8ec4ecc0c983e5c93878539778e226411168a6b
expertmars <expertmars@gmail.com>
2020-09-06 22:47:28 +0530

    Meal app created, added categories

---
+++ lib/category_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import './category_meal_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void _selectCategory(ctx) {
+    Navigator.of(ctx).pushNamed(
+      CategoryMealScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import './category_meal_screen.dart';
+import './categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 1, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 1, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                //  fontWeight: FontWeight.bold,
+              ),
+            ),
+      ),
+      //home: CategoriesScreen(),
+      //initialRoute: "/",
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealScreen.routeName: (ctx) => CategoryMealScreen(),
+      },
+    );
+  }
+}
